fbbcea567ec5c4a16d967d355dcf69e12c03e997
Umeshsuman <umeshsuman14@gmail.com>
2022-05-27 18:37:08 +0530

    27-5 eve

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
-const ExpenseItem = () => {
-    const expenseDate = new Date(2021,3,21);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67;
+const ExpenseItem = (props) => {
     return (
         <div className='expense-item'>
-            <div>{ expenseDate.toISOString() }</div>
+            <ExpenseDate
+                date={props.date}
+            />
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );

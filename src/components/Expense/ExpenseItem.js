61613d64b3bb1a4f8407c9748858672d83049c8d
Umeshsuman <94736870+Umeshsuman@users.noreply.github.com>
2022-06-03 23:04:11 +0530

    Update ExpenseItem.js

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,16 +3,10 @@ import Card from '../UI/card';
 import './ExpenseItem.css'
 import { useState } from 'react';
 const ExpenseItem = (props) => {
-<<<<<<< Updated upstream:src/components/Expense/ExpenseItem.js
-    const [title, setTitle]  = useState(props.title)
-    const clickHandler = () => {
-        setTitle("Updated!!")
-=======
     let title = props.title;
     const handleClick = () =>{
             title = "updated";
         console.log(title)
->>>>>>> Stashed changes:src/components/ExpenseItem.js
     }
     return (
         <Card className='expense-item'>
@@ -21,12 +15,7 @@ const ExpenseItem = (props) => {
             />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
-<<<<<<< Updated upstream:src/components/Expense/ExpenseItem.js
-                <div className='expense-item__price'>${props.amount}</div>
-                <button onClick={clickHandler}>Chnage Title</button>
-=======
                 <div onClick={handleClick} className='expense-item__price'>${props.amount}</div>
->>>>>>> Stashed changes:src/components/ExpenseItem.js
             </div>
         </Card>
     );
